6359981b7f3fb2b91afaa2cc33fd858ea3f68e75
Osakana <47623066+osakana9114@users.noreply.github.com>
2021-07-06 13:49:20 +0900

    feature: コマンド修正、gitignore追加

--- delete-cognito-users.sh
+++ delete-cognito-users.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-POOL_ID={USER_POOL_ID}
-PROFILE={AWS_PROFILE_NAME
+POOL_ID="USER_POOL_ID"
+PROFILE="AWS_PROFILE_NAME"
 
 while read line; do
   line=($line)
 
-  aws cognito-idp admin-delete-user --user-pool-id $POOL_ID --username ${line[0]} --profile $PROFILE
+  aws cognito-idp admin-delete-user --user-pool-id $POOL_ID --username ${line[0]} --profile $PROFILE --region ap-northeast-1
   if test $? -eq 0 ; then
     echo "Deleted: ${line[0]}"
   else
--- list-cognito-users.sh
+++ list-cognito-users.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-POOL_ID={USER_POOL_ID}
-PROFILE={AWS_PROFILE_NAME}
+POOL_ID="USER_POOL_ID"
+PROFILE="AWS_PROFILE_NAME"
 
 while : ; do
-  aws cognito-idp list-users --user-pool-id $POOL_ID --profile $PROFILE $tokenopt | tee tmp | jq -r '.Users[] | [.Username,.Attributes[].Value,.UserCreateDate,.UserLastModifiedDate] | @tsv' >> cognito-users.tsv
+  aws cognito-idp list-users --user-pool-id $POOL_ID --profile $PROFILE --region ap-northeast-1 $tokenopt --output json | tee tmp | jq -r '.Users[] | [.Username,.Attributes[].Value,.UserCreateDate,.UserLastModifiedDate] | @tsv' >> tsv/cognito-users_`date "+%Y%m%d_%H%M%S"`.tsv
   token=`jq -r '.PaginationToken' tmp`
   if [ "$token" == "null" ]; then break; else tokenopt="--pagination-token $token"; fi
 done
 
 rm tmp
+
+## 検索条件 ##
+# UserStatusが "FORCE_CHANGE_PASSWORD" のもの
+# .Users[] | select(.UserStatus == "FORCE_CHANGE_PASSWORD") | [.Username,.Attributes[].Value,.UserCreateDate,.UserLastModifiedDate,.Enabled,.UserStatus] | @tsv
+
+# Attributesから指定した項目のみ抽出
+# .Users[] | [.Username,(.Attributes[] | select(.Name == "foo").Value)] | @tsv
